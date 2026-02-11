1fcc612303a3c378a838238cfe7ade721914d913
maria1218-1218 <madelmar3211@gmail.com>
2026-02-10 20:06:29 -0500

    terminé hasta echo

--- 02_echo/cliente.py
+++ 02_echo/cliente.py
@@ -1,2 +1,16 @@
 # Cliente
+import socket
 
+HOST = 'localhost'
+PORT = 9000
+
+mensaje = input("digite tu mensaje: ")
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+cliente.connect ((HOST, PORT))
+
+cliente.sendall(mensaje.encode())
+print(f"mensaje enviados: '{mensaje}'")
+respuesta = cliente.recv(1024)
+print(f"respuesta del 'Echo': '{respuesta.decode()}'")
+
+cliente.close()
--- 02_echo/servidor.py
+++ 02_echo/servidor.py
@@ -1,2 +1,25 @@
 # Servidor
+import socket
+
+HOST = 'localhost'
+PORT = 9000
+
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind((HOST, PORT))
+servidor.listen ()
+
+while True:
+    print("el servidor 'Echo' esta esperando conexiones ...")
+
+    cliente, direccion = servidor.accept()
+    print(f"un cliente se conecto desde la direccion {direccion}")
+
+    datos = cliente.recv(1024)
+    if not datos:
+        break
+
+
+    print("Datos recibidos: ",datos)
+    cliente.sendall(datos) 
+    cliente.close()
 
