fdd926e52639f9d14804cd3ed2c574a2eef9cc85
maria1218-1218 <madelmar3211@gmail.com>
2026-02-10 20:09:52 -0500

    terminé hasta echo

--- 01_sockets/cliente.py
+++ 01_sockets/cliente.py
@@ -1,1 +1,14 @@
 # Cliente
+import socket
+
+HOST = 'localhost'
+PORT = 9000
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((HOST, PORT))
+
+cliente.sendall(b"Mundo")
+respuesta = cliente.recv(1024)
+print(f"Respuesta del servidor: {respuesta.decode()}")
+
+cliente.close()
--- 01_sockets/servidor.py
+++ 01_sockets/servidor.py
@@ -1,1 +1,17 @@
 # Servidor
+import socket
+
+HOST = 'localhost'
+PORT = 9000
+
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind((HOST, PORT))
+servidor.listen ()
+print("el servidor esta a la espera de conexiones ...")
+
+cliente, direccion = servidor.accept()
+print(f"un cliente se conecto desde la direccion {direccion}")
+
+datos = cliente.recv(1024)
+cliente.sendall(b"Hola!" + datos) # ojo! debe ser binario, no cadena 
+cliente.close()
