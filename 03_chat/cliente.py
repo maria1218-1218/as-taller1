f3c05dbbede00d139170b95fc2efa05bcc35f1a7
Maria del Mar Quigua Ramirez <madelmar3211@gmail.com>
2026-02-17 20:10:54 -0500

    Add client socket implementation with threading
    Implement client functionality to connect to a server, send and receive messages.

--- 03_chat/cliente.py
+++ 03_chat/cliente.py
@@ -1,2 +1,30 @@
 # Cliente
+import socket
+import threading
 
+HOST = 'localhost'
+PORT = 9000
+
+def recibir_mensajes(cliente):
+    while True:
+        mensaje = cliente.recv(1024).decode()
+        print(mensaje)
+
+
+nombre = input("ingrese su nombre: ")
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((HOST, PORT))
+cliente.sendall(nombre.encode())
+
+hilo_recibir = threading.Thread(target = recibir_mensajes, args =(cliente,))
+hilo_recibir.start()
+
+while True:
+    mensaje = input("Mensaje: ")
+    cliente.send(mensaje.encode())
+
+cliente.sendall(b"Mundo!")
+respuesta = cliente.recv(1024)
+print(f"respuesta del servidor: {respuesta.decode()}")
+
+cliente.close()
