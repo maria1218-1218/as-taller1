1de62290c7b59d5d0aef174ecc9c3e6dca87e3e5
Maria del Mar Quigua Ramirez <madelmar3211@gmail.com>
2026-03-03 18:58:00 -0500

    Implement basic HTTP server in servidor.py

--- 04_http/servidor.py
+++ 04_http/servidor.py
@@ -1,2 +1,12 @@
 # Servidor
+import socket
+import http.server
 
+HOST = 'localhost'
+PORT = 9000
+
+class Servidor(http.server.SimpleHTTPRequestHandler):
+    pass
+
+servidor = http.server.HTTPServer((HOST, PORT), Servidor)
+servidor.serve_forever()
