85742af15754a22b5a782465b8c53fa45b323f17
Maria del Mar Quigua Ramirez <madelmar3211@gmail.com>
2026-03-03 18:57:24 -0500

    Add HTTP client implementation in cliente.py

--- 04_http/cliente.py
+++ 04_http/cliente.py
@@ -1,2 +1,16 @@
 # Cliente
+import socket
+import http.client
 
+HOST = 'localhost'
+PORT = 9000
+
+cliente = http.client.HTTPConnection(HOST, PORT)
+cliente.request('GET', '/')
+
+respuesta = cliente.getresponse()
+datos = respuesta.read().decode()
+
+print(datos)
+
+cliente.close()
