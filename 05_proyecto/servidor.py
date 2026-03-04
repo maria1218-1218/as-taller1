89c027f84fc364c032bb3ae46bfc83da789eaa0c
maria1218-1218 <madelmar3211@gmail.com>
2026-03-03 19:18:55 -0500

    trabajo finalizado

--- 05_proyecto/servidor.py
+++ 05_proyecto/servidor.py
@@ -1,2 +1,22 @@
 # Servidor
 
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secreto!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    # Jinja2 renderiza el archivo index.html de la carpeta /templates
+    return render_template('index.html')
+
+@socketio.on('mensaje_cliente')
+def manejar_mensaje(data):
+    print(f"Mensaje de {data['usuario']}: {data['mensaje']}")
+    # Reenviamos el mensaje a todos los conectados
+    emit('mensaje_servidor', data, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=5000)
